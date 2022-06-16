4615e17a73478916a8e226aa76388e7413d77115
Abdullo001 <abdullofayzullayev68@gmail.com>
2022-06-16 20:24:55 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+function addition(a, b) {
+	return a + b;
+}
+
+function convert(minutes) {
+	return minutes * 60;
+}
+
+function addition(num) {
+	return num + 1;
+}
+
+function triArea(base, height) {
+	return (base * height) / 2;
+}
+
+function calcAge(age) {
+	return age * 365;
+}
+
+function remainder(x, y) {
+	return x % y;
+}
+
+function footballPoints(wins, draws, losses) {
+	return wins * 3 + draws;
+}
+
+function points(twoPointers, threePointers) {
+	return twoPointers * 2 + threePointers * 3;
+}
+
+function lessThan100(a, b) {
+	if (a + b < 100) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function animals(chickens, cows, pigs) {
+	return chickens * 2 + cows * 4 + pigs * 4;
+}
